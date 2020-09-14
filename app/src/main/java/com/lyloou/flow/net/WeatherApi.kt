e1f36e77397caaabbeb4d6b56d93938ad125d6da
lilou <lyloou@qq.com>
2020-09-14 16:14:12 +0800

    update: flow url

--- app/src/main/java/com/lyloou/flow/net/WeatherApi.kt
+++ app/src/main/java/com/lyloou/flow/net/WeatherApi.kt
@@ -6,6 +6,8 @@ import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+// [分享免费天气API - 蓄谋已久 - 博客园](https://www.cnblogs.com/liujiuzhou/p/11547156.html)
+// http://wthrcdn.etouch.cn/weather_mini?citykey=101280601
 interface WeatherApi {
     @GET("/weather_mini")
     fun getWeather(@Query("citykey") cityCode: String): Observable<MyWeather>
